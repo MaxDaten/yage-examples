a1bbce558da7419105a13c317243e21af42d86d7
Jan-Philip Loos <maxdaten@gmail.com>
2013-12-23 23:21:49 +0100

    moved basic wires to yage, updated exampes for presentation

--- src/YageWireCube.hs
+++ src/YageWireCube.hs
@@ -7,15 +7,9 @@ module Main where
 import Yage hiding (Event, at, key)
 import Yage.Rendering
 import Yage.Math
+import Yage.Wire
 
 import Yage.Examples.Shared
-
-import Data.Foldable
-import Control.Wire
-import Control.Wire.Unsafe.Event
-import FRP.Netwire.Move
-
-
 
 
 settings :: WindowConfig
@@ -42,22 +36,22 @@ data Cube = Cube
     { _cubePosition    :: V3 Float 
     , _cubeOrientation :: Quaternion Float
     , _cubeScale       :: V3 Float
-    } deriving (Show)
+    }
+    deriving (Show)
 makeLenses ''Cube
 
 
 main :: IO ()
-main = yageMain "MainWire" settings mainWire clockSession
+main = yageMain "yage-cube" settings mainWire clockSession
 
-initialCamOrientation = axisAngle xAxis (deg2rad (-60))
 
 mainWire :: (Real t) => YageWire t () CubeView
 mainWire = proc () -> do
     cubeRot   <- cubeRotationByInput   -< ()
     cameraPos <- cameraMovementByInput -< ()
-    cameraRot <- cameraRotationByInput initialCamOrientation -< ()
+    cameraRot <- cameraRotationByInput -< ()
     returnA -< CubeView 
-                    (fpsCamera & cameraLocation    .~ cameraRot `rotate` (V3 0 1 8 + cameraPos)
+                    (fpsCamera & cameraLocation    .~ cameraRot `rotate` (V3 0 2 8 + cameraPos)
                                & cameraOrientation .~ cameraRot)
                     (Cube (V3 0 1 0) cubeRot 1)
 
@@ -72,14 +66,15 @@ mainWire = proc () -> do
         . 1
 
 
+
     cameraMovementByInput :: (Real t) => YageWire t a (V3 Float)
     cameraMovementByInput = 
         let acc         = 20
             att         = 0.8
             toLeft      = -xAxis
-            toRight     = xAxis
+            toRight     =  xAxis
             forward     = -zAxis
-            backward    = zAxis
+            backward    =  zAxis
         in smoothTranslation forward  acc att Key'W
          . smoothTranslation toLeft   acc att Key'A
          . smoothTranslation backward acc att Key'S
@@ -87,19 +82,9 @@ mainWire = proc () -> do
          . 0
 
 
-    cameraRotationByInput :: (Real t) => Quaternion Float -> YageWire t a (Quaternion Float)
-    cameraRotationByInput initial =
-        let upward                  = V3 0 1 0
-            rightward               = V3 1 0 0
-        in rotationByVelocity upward rightward . arr(/1000) . arr traceShow' . meassureMouseVelocity
-
-    meassureMouseVelocity :: (Real t) => YageWire t a (V2 Float)
-    meassureMouseVelocity = 
-        while . keyDown Key'LeftShift . mouseVelocity <|> 0
-
 
     smoothTranslation :: (Real t)
-                       => V3 Float -> Float -> Float -> Key -> YageWire t (V3 Float) (V3 Float)
+                      => V3 Float -> Float -> Float -> Key -> YageWire t (V3 Float) (V3 Float)
     smoothTranslation dir acc att key =
         let trans = integral 0 . arr (signorm dir ^*) . velocity acc att key
         in proc inTransV -> do
@@ -107,11 +92,32 @@ mainWire = proc () -> do
             returnA -< inTransV + transV
 
 
+    velocity :: (Floating b, Ord b, Real t) 
+             => b -> b -> Key -> YageWire t a b
+    velocity acc att trigger = 
+        integrateAttenuated att 0 . (while . keyDown trigger . pure acc <|> 0)
+
+
+---------------------------------------------------------------------------------------------------
+
+
+    cameraRotationByInput :: (Real t) => YageWire t a (Quaternion Float)
+    cameraRotationByInput =
+        let upward                  = V3 0 1 0
+            rightward               = V3 1 0 0
+        in rotationByVelocity upward rightward . arr(/1000) . meassureMouseVelocity (while . keyDown Key'LeftShift)
+
+
+    -- meassureMouseVelocity :: (Real t) => YageWire t' a a -> YageWire t a (V2 Float)
+    meassureMouseVelocity when' = 
+        when' . mouseVelocity <|> 0
+
+
     smoothRotationByKey :: (Real t) 
-                   => (V3 Float, Float) -> Key -> YageWire t (Quaternion Float) (Quaternion Float)
+                        => (V3 Float, Float) -> Key -> YageWire t (Quaternion Float) (Quaternion Float)
     smoothRotationByKey (axis, dir) key = 
         let acc         = 20
-            att         = 0.85
+            att         = 0.87
             angleVel    = velocity acc att key
             rot         = axisAngle axis <$> integral 0 . arr (dir*) . angleVel
         in proc inQ -> do
@@ -125,79 +131,6 @@ mainWire = proc () -> do
             combineOrientations = arr (\(qu, qr) -> qu * qr)
         in combineOrientations . applyOrientations . integral 0
 
-
-    velocity :: (Floating b, Ord b, Real t) 
-             => b -> b -> Key -> YageWire t a b
-    velocity acc att trigger = 
-        integrateAttenuated att 0 . (while . keyDown trigger . pure acc <|> 0) --- --> pure acc <|> pure decc)
-
-
-
-
-mouseVelocity :: (Real t) => YageWire t a (V2 Float)
-mouseVelocity = derivativeF . arr traceShow' . currentMousePosition
-
-
-mouseAcceleration :: (Real t) => YageWire t a (V2 Float)
-mouseAcceleration = derivativeF . mouseVelocity
-
-
-while :: YageWire t (Event a) a
-while = mkPure_ $ event (Left mempty) Right
-
-keyDown :: (Real t) => Key -> YageWire t a (Event a)
-keyDown key = 
-    mkSF $ \(Timed _ (i, _)) x ->
-        if i `isPressed` key
-            then (Event x, keyDown key)
-            else (NoEvent, keyDown key)
-
-whenKeyUp :: (Real t) => Key -> YageWire t a YageInput
-whenKeyUp key = arr traceShow' . unless (\(i, _) -> i `isPressed` key) . currentInputState
-
-
-currentMousePosition :: (Real t, Fractional b) => YageWire t a (V2 b)
-currentMousePosition = mkSF_ (\(inputSt,_) -> realToFrac <$> (uncurry V2 $ inputSt^.mouse.mousePosition)) . currentInputState
-
-currentInputState :: (Num t) => YageWire t a YageInput
-currentInputState = mkSF $ \(Timed _ s) _ -> (s, currentInputState)
-
-
-
--------------------------------------------------------------------------------
--- Generic Wires
-
-
-integrateAttenuated :: (Floating a, Ord a, HasTime t s) 
-                 => a -> a -> Wire s e m a a
-integrateAttenuated a@attenuation x' = mkPure $ \ds dx ->
-    let dt = realToFrac (dtime ds)
-        x  = a * (x' + dt * dx)
-    in x' `seq` ( Right x', integrateAttenuated a x )
-
-
-integrateBounded :: (Floating a, Ord a, HasTime t s) 
-                 => (a, a) -> a -> Wire s e m a a
-integrateBounded b@(lower,upper) x' = mkPure $ \ds dx ->
-    let dt = realToFrac (dtime ds)
-        x  = x' + dt * dx
-        n  = min upper . max lower $ x
-    in x' `seq` ( Right x', integrateBounded b n )
-
-
-
-derivativeF :: (Foldable f, Fractional (f a), RealFloat a, HasTime t s, Monoid e)
-            => Wire s e m (f a) (f a)
-derivativeF = mkPure $ \_ x -> (Left mempty, loop x)
-    where
-    loop x' = 
-        mkPure $ \ds x ->
-            let dt  = realToFrac (dtime ds)
-                dx  = (x - x') / dt
-                mdx | any isNaN dx       = Right 0
-                    | any isInfinite dx  = Left mempty
-                    | otherwise          = Right dx
-            in mdx `seq` (mdx, loop x)
 
 
 
