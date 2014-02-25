e801d08b72f90b3ed884e68b1ba0f4591a084f1f
Jan-Philip Loos <maxdaten@gmail.com>
2014-02-25 06:45:07 +0100

    adopt selective vertex resource

--- src/Yage/Examples/Shared.hs
+++ src/Yage/Examples/Shared.hs
@@ -1,18 +1,15 @@
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# LANGUAGE PackageImports #-}
 module Yage.Examples.Shared
     ( module Yage.Examples.Shared
-    , module Prims
     ) where
 
 import Yage.Prelude hiding (Text)
 
+
 import Yage.Rendering hiding (P3, P3N3)
 import Yage.Primitives
-import Yage.Primitives as Prims (NormalSmoothness(..))
-
-import           Yage.Vertex
-
-
+import "yage" Yage.Geometry
 
 
 ---------------------------------------------------------------------------------------------------
@@ -22,7 +19,7 @@ sphereEntity :: Int -> RenderEntity P3N3
 sphereEntity subdivides =
     let mesh      = (vertices . triangles $ normalCalculator SphericalNormals $ geoSphere subdivides 0.5) :: [Vertex P3N3]
         rdef      = RenderDefinition
-            { _rdefData     = makeMesh "sphere" mesh
+            { _rdefData     = Right $ makeMesh "sphere" mesh
             , _rdefTextures = []
             , _rdefMode     = Triangles
             }
@@ -33,7 +30,7 @@ coneEntity :: Int -> RenderEntity P3N3
 coneEntity divs =
     let mesh      = (vertices . triangles $ normalCalculator SphericalNormals $ cone 0.5 1 divs) :: [Vertex P3N3]
         rdef      = RenderDefinition
-            { _rdefData     = makeMesh "cone" mesh
+            { _rdefData     = Right $ makeMesh "cone" mesh
             , _rdefTextures = []
             , _rdefMode     = Triangles
             }
@@ -43,7 +40,7 @@ pyramidEntity :: RenderEntity P3N3
 pyramidEntity =
     let mesh      = (vertices . triangles $ normalCalculator FacetteNormals $ pyramid 1) :: [Vertex P3N3]
         rdef      = RenderDefinition
-            { _rdefData     = makeMesh "pyramid" mesh
+            { _rdefData     = Right $ makeMesh "pyramid" mesh
             , _rdefTextures = []
             , _rdefMode     = Triangles
             }
@@ -54,7 +51,7 @@ boxEntity :: RenderEntity P3N3
 boxEntity =
     let mesh      = (vertices . triangles $ normalCalculator FacetteNormals $ cube 1) :: [Vertex P3N3]
         rdef      = RenderDefinition
-            { _rdefData     = makeMesh "cube" mesh
+            { _rdefData     = Right $ makeMesh "cube" mesh
             , _rdefTextures = [ TextureDefinition (0, "textures")
                                (TextureFile ("res" </> "Brown_Leather_Texture.png"))
                               ]
@@ -67,9 +64,20 @@ floorEntity :: RenderEntity P3N3
 floorEntity =
     let mesh        = vertices . toLines $ normalCalculator FacetteNormals $ grid 100 1 :: [Vertex P3N3]
         rdef        = RenderDefinition
-                        { _rdefData     = makeMesh "floor" mesh
+                        { _rdefData     = Right $ makeMesh "floor" mesh
                         , _rdefTextures = []
                         , _rdefMode     = Lines
+                        }
+    in mkRenderEntity rdef
+
+objEntity :: OBJ -> RenderEntity P3N3
+objEntity obj =
+    let mesh         = geoElements $ loadObj obj :: Vector (Vertex P3N3)
+        Just theName = obj^.name <|> Just "unnamed obj"
+        rdef         = RenderDefinition
+                        { _rdefData     = Right $ makeMeshV theName mesh
+                        , _rdefTextures = []
+                        , _rdefMode     = Triangles
                         }
     in mkRenderEntity rdef
 
