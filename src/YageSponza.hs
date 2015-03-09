b9a512ae9a7219f2744927d6399fee9ba28e8600
Jan-Philip Loos <maxdaten@gmail.com>
2015-03-09 20:46:04 +0100

    sponza compiled

--- src/YageSponza.hs
+++ src/YageSponza.hs
@@ -11,121 +11,142 @@
 
 module Main where
 
-import Yage
+import Yage hiding ((><), point)
 import Yage.Lens hiding ((<.>))
-import Yage.Math
+import Yage.Math hiding (normal, (><), point)
 import Yage.Wire hiding ((<>))
+import Yage.Wire.Utils
 
 import Yage.Camera
 import Yage.Scene
+import qualified Yage.Vertex as V
 import Yage.HDR
-import Yage.UI.GUI
-import Yage.Texture
+import Yage.UI.GUI hiding (Texture)
 import Yage.Transformation
 import qualified Yage.Resources as Res
-import qualified Yage.Material  as Mat
-import Yage.Pipeline.Deferred
+import Yage.Material as Mat
+import Yage.Rendering.Resources.GL
+import Yage.Rendering.Pipeline.Deferred
+import Yage.Rendering.Pipeline.Deferred.SkyPass
+import Yage.Formats.Ygm
 import Yage.Examples.Shared
-
-winSettings :: WindowConfig
-winSettings = WindowConfig
-    { windowSize = (1200, 800)
-    , windowHints =
-        [ WindowHint'ContextVersionMajor  4
-        , WindowHint'ContextVersionMinor  1
-        , WindowHint'OpenGLProfile        OpenGLProfile'Core
-        , WindowHint'OpenGLForwardCompat  True
-        , WindowHint'RefreshRate          60
-        --, WindowHint'Resizable            False
-        --, WindowHint'Decorated            False
-        ]
-     }
-
-
-appConf :: ApplicationConfig
-appConf = defaultAppConfig{ logPriority = WARNING }
-
-main :: IO ()
-main = yageMain "yage-sponza" appConf winSettings mainWire yDeferredLighting (1/60)
-
+import Data.Traversable (sequenceA)
+import qualified Data.Sequence as Seq
+import Data.Sequence ((><))
 
 -------------------------------------------------------------------------------
--- View Definition
+-- * Scene Definition
 
 
-type SceneEntity      = GeoEntity
-type SceneEnvironment = Environment Light SkyEntity
-type SponzaScene      = Scene HDRCamera SceneEntity SceneEnvironment GUI
+data SponzaScene = SponzaScene
+  { _sponzaScene          :: DeferredScene
+  , _sponzaCamera         :: HDRCamera
+  , _sponzaRenderSettings :: DeferredSettings
+  }
+
+makeLenses ''SponzaScene
 
 mainWire :: (HasTime Double (YageTimedInputState t), Real t) => YageWire t () SponzaScene
 mainWire = proc () -> do
-    world <- worldEntityW -< ()
-    cam   <- hdrCameraHandle `overA` cameraControl -< camera
-    sky   <- skyDomeW -< cam^.hdrCameraHandle.cameraLocation
-    returnA -< emptyScene cam emptyGUI
-                    & sceneLights   .~ fromList (genLights baseLight)
-                    & sceneEntities .~ fromList [ world ]
-                    & sceneSky ?~ sky
-    where
-    texDir      = "res" </> "tex"
+    world   <- worldEntityW -< ()
+    hdrCam  <- hdrCameraW -< ()
+    skyDome <- skyDomeW -< hdrCam^.camera.position
+    deferreConfig <- deferredSettingsController -< def
 
-    sponzaModel = meshRes $ loadYGM geoVertex $ ( "res" </> "model" </> "env" </> "sponza.ygm", mkSelection [] )
+    let env = emptyEnvironment & sky ?~ skyDome & lights.point .~ singleton directionalLight
+    returnA -< SponzaScene
+      { _sponzaScene          = Scene (singleton world) env (Box (-100) 100)
+      , _sponzaCamera         = hdrCam
+      , _sponzaRenderSettings = deferreConfig
+      }
+ where
+  directionalLight = makeDirectionalLight (V3 (-1) (-1) (-1)) (V3 1 0.953 0.918) 0.0
+  baseLight pos = makePointlight pos 10 (V3 1.0 1.0 1.0) 0.1
+  genLights mkLight = map mkLight
+    [ V3 (5 * x - 2.5) (5 * y) (5 * z - 2.5)
+    | x <- [0..2]
+    , y <- [0..2]
+    , z <- [0..2]
+    ]
 
-    worldEntityW =
-        let albedoTex = mkTexture2D "Albedo" <$> (imageRes $ texDir</>"default"<.>"png")
-            normalTex = mkTexture2D "Normal" <$> (imageRes $ texDir</>"floor_n"<.>"png")
-        in proc () -> do
-            entity <- renderData <~~ constMeshW sponzaModel
-                >>> materials.albedoMaterial.Mat.matTexture <~~ constTextureW albedoTex
-                >>> materials.normalMaterial.Mat.matTexture <~~ constTextureW normalTex -< basicEntity :: SceneEntity
+-- * World
 
-            returnA -< entity & materials.albedoMaterial.Mat.stpFactor .~ 2.0
-                              & materials.normalMaterial.Mat.stpFactor .~ 2.0
-                              & entityScale //~ 200
+worldEntityW :: YageWire t () DeferredEntity
+worldEntityW = acquireOnce sponzaEntity <&> transformation.scale //~ 400
+ where
+  sponzaEntity :: YageResource DeferredEntity
+  sponzaEntity = Entity
+    <$> (fromMesh =<< sponzaMesh) <*> sponzaMaterial <*> pure idTransformation
 
-    baseLight p = Light
-                    { _lightType      = Pointlight p 10
-                    , _lightColor     = V3 1.0 1.0 1.0
-                    , _lightIntensity = 0.1
-                    }
+  sponzaMaterial :: YageResource (GBaseMaterial Texture2D)
+  sponzaMaterial = do
+    albedoTex <- textureRes =<< (imageRes $ "res"</>"tex"</>"default"<.>"png")
+    normalTex <- textureRes =<< (imageRes $ "res"</>"tex"</>"floor_n"<.>"png")
+    gBaseMaterialRes defaultGBaseMaterial
+      <&> albedo.materialTexture     .~ albedoTex
+      <&> normalmap.materialTexture  .~ normalTex
+      <&> albedo.stpFactor           .~ 2.0
+      <&> normalmap.stpFactor        .~ 2.0
+      <&> roughness.materialColor    .~ 0.5
+
+  sponzaMesh :: YageResource (Mesh YGMVertex)
+  sponzaMesh = meshRes $ loadYGM id ( "res" </> "model" </> "env" </> "sponza.ygm", mkSelection [] )
+
+-- * Sky Dome
+
+skyDomeW :: Real t => YageWire t (V3 Double) DeferredSky
+skyDomeW = proc pos -> do
+  skye <- acquireOnce skyEntity -< ()
+  returnA -< skye
+    & transformation.position  .~ pos
+    & transformation.scale     .~ 50
+
+ where
+  skyEntity :: YageResource DeferredSky
+  skyEntity = Entity <$> fromMesh skydome <*> skyMaterial <*> pure idTransformation
+  skyMaterial :: YageResource (SkyMaterial TextureCube)
+  skyMaterial = do
+    -- (imageRes $ texDir</>"misc"</>"blueprint"</>"Seamless Blueprint Textures"</>"1"<.>"png")
+    envMap <- (textureRes =<< (cubeCrossMipsRes Strip ("res"</>"tex"</>"env"</>"Sea"</>"small"</>"strip_half.jpg")))
+    radMap <- (textureRes =<< (cubeCrossMipsRes Strip ("res"</>"tex"</>"env"</>"Sea"</>"pmrem"</>"*_m<->.png")))
+    -- radMap <- textureRes (sameFaces $ blackDummy :: Cubemap (Image PixelRGB8))
+    -- envMap <- textureRes (sameFaces $ blackDummy :: Cubemap (Image PixelRGB8))
+    return $ SkyMaterial (defaultMaterialSRGB & materialTexture .~ envMap)
+                         (defaultMaterialSRGB & materialTexture .~ radMap)
 
 
-    skyDomeW :: YageWire t (V3 Double) SkyEntity
-    skyDomeW = proc pos -> do
-        tex <- cubeTextureToTexture "SkyCube" . pure <$> constTextureW skyTex -< ()
-        returnA -< skydome & materials.skyEnvironmentMap
-                                      .Mat.matTexture .~ tex
-                           & entityPosition           .~ pos
-                           & entityScale              .~ 50
+-- * Camera Control
 
-    skyTex  = mkTexture2D "SkyTexture" <$> (imageRes $ texDir</>"misc"</>"blueprint"</>"Seamless Blueprint Textures"</>"1"<.>"png")
+hdrCameraW :: Real t => YageWire t () HDRCamera
+hdrCameraW =
+  let initCamera = idCamera (deg2rad 75) 0.1 500
+  in hdrController . (defaultHDRCamera <$> cameraControl . pure initCamera)
 
-    camera          = defaultHDRCamera ( mkCameraFps (deg2rad 75) (0.1,10000) )
-                        & hdrExposure           .~ 2
-                        & hdrExposureBias       .~ 0.0
-                        & hdrWhitePoint         .~ 11.2
-                        & hdrBloomSettings      .~ bloomSettings
+hdrController :: Num t => YageWire t HDRCamera HDRCamera
+hdrController =
+  exposure      <~~ ( spin (0, 10) 1.0 <<< (( 0.05 <$) <$> keyJustPressed Key'Period)  &&&
+                                           ((-0.05 <$) <$> keyJustPressed Key'Comma) ) >>>
 
-    bloomSettings   = defaultBloomSettings
-                        & bloomFactor           .~ 0.7
-                        & bloomPreDownsampling  .~ 2
-                        & bloomGaussPasses      .~ 5
-                        & bloomWidth            .~ 2
-                        & bloomThreshold        .~ 0.5
+  exposureBias  <~~ ( spin (-10, 10) 0.0 <<< (( 0.01 <$) <$> keyJustPressed Key'M)     &&&
+                                             ((-0.01 <$) <$> keyJustPressed Key'N) ) >>>
+  whitePoint    <~~ pure 11.2       >>>
+  bloomSettings <~~ pure bloomConfig
+ where
+  bloomConfig :: HDRBloomSettings
+  bloomConfig = defaultBloomSettings
+    & bloomFactor           .~ 0.7
+    & bloomPreDownsampling  .~ 4
+    & bloomGaussPasses      .~ 7
+    & bloomWidth            .~ 1
+    & bloomThreshold        .~ 0.5
 
-
-    genLights mkLight = map mkLight [ V3 (5 * x - 2.5) (5 * y) (5 * z - 2.5)
-                                    | x <- [0..2]
-                                    , y <- [0..2]
-                                    , z <- [0..2]
-                                    ]
 
 
 camStartPos :: V3 Double
 camStartPos = V3 0 2 2
 
 mouseSensitivity :: V2 Double
-mouseSensitivity = V2 0.1 0.1
+mouseSensitivity = V2 (pi/500) (pi/500)
 
 wasdControlled :: Real t => YageWire t () (V3 Double)
 wasdControlled = wasdMovement (V2 2 2)
@@ -135,4 +156,69 @@ mouseControlled = whileKeyDown Key'LeftControl . arr (mouseSensitivity *) . mous
 
 cameraControl :: Real t => YageWire t Camera Camera
 cameraControl = fpsCameraMovement camStartPos wasdControlled . fpsCameraRotation mouseControlled
+
+deferredSettingsController :: Num t => YageWire t DeferredSettings DeferredSettings
+deferredSettingsController =
+  overA showDebugOverlay (toggle (keyJustReleased Key'F12) False True)
+  . overA activeVoxelAmbientOcclusion (toggle (keyJustReleased Key'F9) True False)
+
+-- * Boilerplate
+
+
+winSettings :: WindowConfig
+winSettings = WindowConfig
+    { windowSize = (1600, 1000)
+    , windowHints =
+        [ WindowHint'ContextVersionMajor  4
+        , WindowHint'ContextVersionMinor  4
+        , WindowHint'OpenGLProfile        OpenGLProfile'Core
+        , WindowHint'OpenGLForwardCompat  True
+        , WindowHint'RefreshRate          60
+        , WindowHint'sRGBCapable          True
+        , WindowHint'OpenGLDebugContext   True
+        -- , WindowHint'Resizable            False
+        , WindowHint'Decorated            True
+        ]
+     }
+
+data Configuration = Configuration
+  { _mainAppConfig      :: ApplicationConfig
+  , _mainWindowConfig   :: WindowConfig
+  , _mainMonitorOptions :: MonitorOptions
+  }
+
+makeLenses ''Configuration
+
+appConf :: ApplicationConfig
+appConf = defaultAppConfig{ logPriority = INFO }
+
+configuration :: Configuration
+configuration = Configuration appConf winSettings (MonitorOptions "localhost" 8080 True False)
+
+main :: IO ()
+main = yageMain "yage-sponza" configuration mainWire yDeferredLighting (1/60)
+
+instance HasMonitorOptions Configuration where
+  monitorOptions = mainMonitorOptions
+
+instance HasWindowConfig Configuration where
+  windowConfig = mainWindowConfig
+
+instance HasApplicationConfig Configuration where
+  applicationConfig = mainAppConfig
+
+instance HasScene SponzaScene DeferredEntity DeferredEnvironment where
+  scene = sponzaScene
+
+instance HasHDRCamera SponzaScene where
+  hdrCamera = sponzaCamera
+
+instance HasEntities SponzaScene (Seq DeferredEntity) where
+  entities = sponzaScene.entities
+
+instance HasDeferredSettings SponzaScene where
+  deferredSettings = sponzaRenderSettings
+
+instance LinearInterpolatable SponzaScene where
+  lerp _ _ = id
 
